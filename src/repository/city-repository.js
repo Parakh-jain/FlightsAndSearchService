528175cc7c3675944ec24f0ae39de106cd2280c3
Parakh Jain <parakhjainwork@gmail.com>
2023-04-09 17:01:05 +0530

    Completed city repository

--- src/repository/city-repository.js
+++ src/repository/city-repository.js
@@ -3,10 +3,13 @@ const {City} = require('../models/index');
 class CityRepository {
     async createCity({ name }) {
         try {
-            const city = await City.create({name});
+            const city = await City.create({
+                name
+            });
             return city;
         }
         catch (error) {
+            console.log("Something went wrong in the repository layer");
             throw {error};
         }
     }
@@ -20,6 +23,34 @@ class CityRepository {
             });
         }
         catch (error) {
+            console.log("Something went wrong in the repository layer");
+            throw {error};
+        }
+    }
+
+    async updateCity(cityId, data) {
+        try {
+            // First Way to update
+            const city = await City.update(data, {
+                where: {
+                    id: cityId
+                }
+            });
+            return city;
+        } catch (error) {
+            console.log("Something went wrong in the repository layer");
+            throw {error};
+        }
+    }
+
+    async getCity(cityId) {
+        try {
+            const city = await City.findByPk({
+                cityId
+            });
+            return city;
+        } catch (error) {
+            console.log("Something went wrong in the repository layer");
             throw {error};
         }
     }
